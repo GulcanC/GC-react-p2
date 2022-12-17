afff4ef2d623b6c318892b7bc68415be9368d192
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2022-12-17 21:09:00 +0100

    some correction about grdi system

---
+++ src/components/slide.jsx
@@ -0,0 +1,2 @@
+// The react useState hook allwos us to track state in a function component
+// The useState hook can be used to keep track of strings, numbers, booleans, arrays, objects and any combination of these.
